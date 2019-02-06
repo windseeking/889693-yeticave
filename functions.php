8a0d60b6623083f7280353a8926389c2d31b8b40
windseeking <windseeking2@gmail.com>
2019-02-06 11:50:42 +0300

    Задание #7: шаблонизация

--- functions.php
+++ functions.php
@@ -1,9 +1,32 @@
 <?php
 
+$is_auth = rand(0, 1);
+
+function filter_tags(string $str = null): string {
+    return $str === null ? '' : strip_tags($str);
+};
+
 function format_cost(float $cost): string {
     $cost = ceil($cost);
     if ($cost >= 1000) {
         $cost = number_format($cost, 0, ',', ' ');
     }
     return $cost . ' ₽';
+};
+
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!is_readable($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require $name;
+
+    $result = ob_get_clean();
+
+    return $result;
 };
